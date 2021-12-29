8261a97e584bc0143ced99595a4ceccb9a783b4d
Luis Fernando Alvarez <lalvarez@stackbuilders.com>
2021-12-29 05:33:42 -0500

    Implement authorization

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, UseGuards } from "@nestjs/common";
+import { AuthorizationGuard } from "src/authorization/authorization.guard";
 import { Message } from "../models/messages";
 import { MessagesService } from "./messages.service";
 
@@ -7,17 +8,19 @@ export class MessagesController {
   constructor(private readonly messagesService: MessagesService) {}
 
   @Get("public")
-  async getPublic(): Promise<SuccessMessage> {
+  async getPublic(): Promise<Message> {
     return this.messagesService.getPublicMessage();
   }
 
+  @UseGuards(AuthorizationGuard)
   @Get("protected")
-  async getProtected(): Promise<SuccessMessage> {
+  async getProtected(): Promise<Message> {
     return this.messagesService.getProtectedMessage();
   }
 
+  @UseGuards(AuthorizationGuard)
   @Get("admin")
-  async getAdmin(): Promise<SuccessMessage> {
+  async getAdmin(): Promise<Message> {
     return this.messagesService.getAdminMessage();
   }
 }
